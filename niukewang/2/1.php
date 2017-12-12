45e600fb21b03defa0c22d4e0ca9d0f2ca089cb7
mygphp <2544549156@qq.com>
2017-12-12 16:21:07 +0800

    网易和美团习题

---
+++ niukewang/2/1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/12
+ * Time: 15:51
+ */
+header("Content-type: text/html; charset=utf-8");
+echo "<h2>php工程师的能力评估</h2>";
+echo "<br/>";
